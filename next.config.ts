cbe23b8cd0152adc9160cbd7fbbbf0c9b227abca
Ahmed Morgan <41932609+ahmedmorgan86@users.noreply.github.com>
2026-09-03 18:51:09 +0300

    new

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "http://172.16.20.249:3000/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
